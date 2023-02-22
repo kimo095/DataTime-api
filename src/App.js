dea5a4743e9430ed49b34288fb81c6a1c91007c1
kimo095 <eng095karim@gmail.com>
2023-02-22 16:36:32 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [timeData, setTimeData] = useState(null);
+
+  useEffect(() => {
+    const fetchTimeData = async () => {
+      const response = await fetch('http://worldtimeapi.org/api/timezone/Europe/Amsterdam');
+      const data = await response.json();
+      setTimeData(data);
+    };
+    fetchTimeData();
+    const interval = setInterval(() => fetchTimeData(), 4000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {timeData ? (
+        <div className="central-area">
+          <pre>{JSON.stringify(timeData, null, 2)}</pre>
+        </div>
+      ) : (
+        <div>Loading...</div>
+      )}
     </div>
   );
 }
